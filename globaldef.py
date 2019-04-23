9c73238ef54742be5f9f64f0da16e0e43f87da9f
lycanthropes <38000321+lycanthropes@users.noreply.github.com>
2019-04-23 14:53:14 +0800

    Delete globaldef.py

--- globaldef.py
+++
@@ -1,9 +0,0 @@
-# define globaldef variables
-
-secIDNum = 2
-indIDNum = 5
-secID = [0] * secIDNum
-indID = [0] * indIDNum
-gdata = [[0 for col in range(indIDNum)] for row in range(secIDNum)]
-latest_price=[0,0]
-
